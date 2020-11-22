59465620dc077ab5a26c97e625dfde7572945bb9
marion emeb <marion.emeric@gmail.com>
2020-11-22 15:17:19 +0100

    update css

--- src/View/display.php
+++ src/View/display.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width,">
         <link rel="stylesheet" href="/assets/css/display.css">
         <link rel="stylesheet" href="/assets/css/dropdown.css">
+        <link rel="stylesheet" href="/assets/css/title.css">
         <title>Affichage des données</title>
     </head>
     <body>
         <div class="container">
-            <h1>Acquisition et stockage des données</h1>
+            <h1 class="home-title"><span>Visualisation des données</span></h1>
             <a href="/home/index"><h2>Importer de nouvelles données</h2></a>
 
             <!-- Display one row by date -->
@@ -27,35 +28,35 @@
                 <thead data-toggle="dropdown" class="toggle">
                     <tr>
                         <th class="text-left" colspan="6"><?= $date['date_expedition'] ?></th>
-                        <th class="text-right" colspan="1">Total: <?= $sum ?> EUR</th>
+                        <th class="text-right" colspan="1">Total: <?= $sum ?> €</th>
                         <th class="text-left" colspan="1"><i class="icon"></i></th>
                     </tr>
                 </thead>
                 <tbody class="table-hover dropdown-menu">
                     <!-- Columns titles -->
                     <tr>
-                        <th class="text-left">id</th>
-                        <th class="text-left">Raison sociale expéditeur</th>
-                        <th class="text-left">Date expédition colis</th>
-                        <th class="text-left">N° envoi</th>
-                        <th class="text-left">Raison Sociale destinataire</th>
-                        <th class="text-left">Votre référence</th>
-                        <th class="text-left">Bon de transport</th>
-                        <th class="text-left">Montant h.t. Total</th>
+                        <th class="text-center">id</th>
+                        <th class="text-center">Raison sociale expéditeur</th>
+                        <th class="text-center">Date expédition colis</th>
+                        <th class="text-center">N° envoi</th>
+                        <th class="text-center">Raison Sociale destinataire</th>
+                        <th class="text-center">Votre référence</th>
+                        <th class="text-center">Bon de transport</th>
+                        <th class="text-center">Montant h.t. Total</th>
                     </tr>
                 
                     <!-- one row by data -->
                     <?php foreach($datas as $data): ?>
                         <?php if(!empty($data) and $data['date_expedition'] === $date['date_expedition']): ?>
                         <tr>
-                            <td class="text-left"><?= $data['id'] ?></td>
-                            <td class="text-left"><?= $data['expediteur'] ?></td>
-                            <td class="text-left"><?= $data['date_expedition'] ?></td>
-                            <td class="text-left"><?= $data['numero_envoi'] ?></td>
-                            <td class="text-left"><?= $data['destinataire'] ?></td>
-                            <td class="text-left"><?= $data['reference'] ?></td>
-                            <td class="text-left"><?= $data['bon_de_transport'] ?></td>
-                            <td class="text-left"><?= $data['montant_ht_total'] ?></td>
+                            <td class="text-center"><?= $data['id'] ?></td>
+                            <td class="text-center"><?= $data['expediteur'] ?></td>
+                            <td class="text-center"><?= $data['date_expedition'] ?></td>
+                            <td class="text-center"><?= $data['numero_envoi'] ?></td>
+                            <td class="text-center"><?= $data['destinataire'] ?></td>
+                            <td class="text-center"><?= $data['reference'] ?></td>
+                            <td class="text-center"><?= $data['bon_de_transport'] ?></td>
+                            <td class="text-center"><?= $data['montant_ht_total'] ?></td>
                         </tr>
                         <?php endif; ?>
                     <?php endforeach; ?>
--- src/View/welcome.php
+++ src/View/welcome.php
@@ -3,21 +3,23 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="/assets/css/welcome.css">
+        <link rel="stylesheet" href="/assets/css/title.css">
+        <link href="/assets/css/all.css" rel="stylesheet">
         <title>Acquisition et stockage des données</title>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <img src="/assets/images/logo-3.jpg">
-                <h1>Acquisition et stockage des données</h1>
+                <h1 class="home-title"><span>Acquisition et stockage des données</span></h1>
                 
-                <form action="/loadCsv/loadCsv" method="post" enctype="multipart/form-data" class="box">
+                <form action="/loadCsv/loadCsv" method="post" enctype="multipart/form-data" class="box card">
                 <img src="/assets/images/upload.png">
-                    <div>Sélectionner un fichier ".csv":</div>
+                    <div>Sélectionner un fichier <b>CSV</b></div>
                     <input type="file" name="file" id="file">
                     <input type="submit" value="Télécharger" name="submit" class="button">
                 </form>
-                <a href="/data/index" class="arrow"><h1>Afficher les données</h1><img src="/assets/images/data.png"></a>
+                <a href="/data/index" class="arrow"><h1>Afficher les données</h1><i class="fas fa-angle-right"></i></a>
             </div>
         </div>
         
